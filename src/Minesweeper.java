2c6146a9a3f46797901a0d2a74fdde9bb7fde116
Tomohiro Matsunaga <lc21-1212@lclark.edu>
2023-02-01 13:52:03 -0800

    incalss 2/1 over

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,5 +1,15 @@
-public class Main {
+public class Minesweeper {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StdOut.println("Welcome to minesweeper!");
+        int size  = 10, numMines = 10;
+        boolean[][] grid = new boolean[size][size];
+        boolean[][] revealed = new boolean[size][size];
+
+        //TODO initialize grid: place bombs in random location
+
+        //TODO 1. draw the grid
+
+        //TODO 2.
+
     }
 }
