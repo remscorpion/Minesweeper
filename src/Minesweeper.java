d5b9a22d4dc1b4e566b76397087f1ea231b3fbde
Tomohiro Matsunaga <lc21-1212@lclark.edu>
2023-02-03 12:01:49 -0800

    inclass 2/3 initialize the code not finish

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -5,11 +5,46 @@ public class Minesweeper {
         boolean[][] grid = new boolean[size][size];
         boolean[][] revealed = new boolean[size][size];
 
+
         //TODO initialize grid: place bombs in random location
+        initMinefield(numMines, grid);
 
-        //TODO 1. draw the grid
+        //TODO 1. draw the grid (take into account revealed)
+        drawMinefield(grid, revealed);
 
-        //TODO 2.
+        //TODO keep doing there steps until otherwise told
+        while (true) {
+            //TODO 2. wait until the user clicks (give coordinates)
 
+            //TODO 3. update "revealed" (based on theses coordinates)
+
+            handleMouseClick(grid, revealed);
+
+            drawMinefield(grid, revealed);
+
+            //TODO 4. check for losing/winning condition
+            if (hasWon(grid, revealed)) {
+                StdOut.println("You Won!");
+                break;
+            }
+        }
+
+
+
+    }
+
+    public static boolean hasWon(boolean[][] grid, boolean[][] revealed) {
+        return false;
+    }
+
+    public static void handleMouseClick(boolean[][] grid, boolean[][] revealed) {
+
+    }
+
+    public static void drawMinefield(boolean[][] grid, boolean[][] revealed) {
+
+    }
+
+    public static void initMinefield(int numMines, boolean[][] grid) {
     }
 }
